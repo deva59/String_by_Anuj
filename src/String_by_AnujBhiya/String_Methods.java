5f851b46d835ea197738d7702d02234eba2afbe0
deva59 <devendrashimpi59@gmail.com>
2024-11-28 21:56:59 +0530

    init Commit

---
+++ src/String_by_AnujBhiya/String_Methods.java
@@ -0,0 +1,37 @@
+package String_by_AnujBhiya;
+
+import java.util.Locale;
+import java.util.*;
+
+public class String_Methods {
+    public static void main(String[] args) {
+        String name = "  Devendra2001 ";
+
+        //toUpperCase()
+       // System.out.println(name.toUpperCase());
+
+        //toLowerCase()
+       // System.out.println(name.toLowerCase());
+
+        //trim() - Remove Spaces
+       // System.out.println(name.trim());
+
+        //charAt() - Index Position Check and Print
+     //   System.out.println(name.charAt(3));
+
+        System.out.println(name.replace("Devendra", "Shimpi"));
+
+
+        //valueOf - datatype convert into string
+       // int age = 123;
+      //String stringAge = String.valueOf(age);
+        //System.out.println(age+2); //125 Print
+        //System.out.println(stringAge+2); //1232 Print Because This Is String
+
+
+
+
+
+
+    }
+}
